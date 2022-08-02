95f6d9bfc0450459a8c2af5b188948ba147f7966
Stephanie Volcov <stephanie.volcov.b@gmail.com>
2022-08-02 16:26:52 -0300

    change favicon and add github pages

--- front-end/Lista-Mercado/src/index.js
+++ front-end/Lista-Mercado/src/index.js
@@ -2,11 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import {BrowserRouter} from 'react-router-dom';
+import {} from 'react-router-dom';
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <App />
     </BrowserRouter>
   </React.StrictMode>,
